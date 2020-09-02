d872677a3f53689b83b60008c24c825caa85875d
Juan Oxmar Ramirez <joxmar.ramirez@gmail.com>
2020-09-01 22:54:13 -0700

    Updates

---
+++ precache-manifest.3272e648f32329b86f81f3eb027d1d80.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4f3d4ae53bf60de10cb0f882a8d5ebf",
+    "url": "/tweeter/index.html"
+  },
+  {
+    "revision": "fc1cb02fd44d4e777c89",
+    "url": "/tweeter/static/css/main.1a81fa31.chunk.css"
+  },
+  {
+    "revision": "610a1bfe8411078e85d3",
+    "url": "/tweeter/static/js/2.e0876e70.chunk.js"
+  },
+  {
+    "revision": "1937a0fc6634269e5f47f8181e27fdc7",
+    "url": "/tweeter/static/js/2.e0876e70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc1cb02fd44d4e777c89",
+    "url": "/tweeter/static/js/main.ed796ffa.chunk.js"
+  },
+  {
+    "revision": "76109fc8aa5b3d7a481f",
+    "url": "/tweeter/static/js/runtime-main.6a247108.js"
+  },
+  {
+    "revision": "a7a925c7c07f163f3274df0c1ab2c321",
+    "url": "/tweeter/static/media/TwitterLogo.a7a925c7.svg"
+  },
+  {
+    "revision": "896129aca3c7e4a523e38f1028762e02",
+    "url": "/tweeter/static/media/avatar.896129ac.svg"
+  },
+  {
+    "revision": "f3101c81c742909fd0f2895aec56d6d7",
+    "url": "/tweeter/static/media/feather.f3101c81.svg"
+  },
+  {
+    "revision": "e9c4061a8cd9bb9308d6d97773556b01",
+    "url": "/tweeter/static/media/home.e9c4061a.svg"
+  },
+  {
+    "revision": "19fa33e63cc49f9d00e5295c721c992c",
+    "url": "/tweeter/static/media/messages.19fa33e6.svg"
+  },
+  {
+    "revision": "6ed7f9f01871f426e979afb4e2252bf2",
+    "url": "/tweeter/static/media/ring.6ed7f9f0.svg"
+  },
+  {
+    "revision": "3061ebade90222c243e2991a5100aa4c",
+    "url": "/tweeter/static/media/search.3061ebad.svg"
+  },
+  {
+    "revision": "f9a093a8161e1a85d3d4dfef52138771",
+    "url": "/tweeter/static/media/user.f9a093a8.svg"
+  }
+]);
